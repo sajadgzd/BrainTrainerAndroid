fe8a6a1b77444662e9dd1284d28fdbbfb8faf4ee
sajad1993gh <sajad1993gh@gmail.com>
2019-11-28 15:15:44 -0500

    finished UI

--- app/src/main/java/com/example/braintrainerandroid/MainActivity.java
+++ app/src/main/java/com/example/braintrainerandroid/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +17,22 @@ public class MainActivity extends AppCompatActivity {
         goButton.setVisibility(View.INVISIBLE);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView sumTextView = findViewById(R.id.sumTextView);
         goButton = findViewById(R.id.goButton);
+
+        Random rand = new Random();
+        int a = rand.nextInt(21); // range 0 to 20
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+
+
     }
 }
