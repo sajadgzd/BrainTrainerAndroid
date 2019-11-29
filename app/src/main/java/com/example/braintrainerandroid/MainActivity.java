f01e5e0390fef1ac7773eae6a24e69ce780b0fad
sajad1993gh <sajad1993gh@gmail.com>
2019-11-28 23:19:08 -0500

    cleaned up code 2

--- app/src/main/java/com/example/braintrainerandroid/MainActivity.java
+++ app/src/main/java/com/example/braintrainerandroid/MainActivity.java
@@ -24,47 +24,8 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    //
-    // Write a message to the database
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
-//         myRef.setValue("Hello, World!");
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("highestScore");
-
-    // Read:
-    // Read from the database
-    //     myRef.addValueEventListener(new ValueEventListener() {
-    //        @Override
-    //        public void onDataChange(DataSnapshot dataSnapshot) {
-    //            // This method is called once with the initial value and again
-    //            // whenever data at this location is updated.
-    //            String value = dataSnapshot.getValue(String.class);
-    //            Log.d(TAG, "Value is: " + value);
-    //        }
-    //
-    //        @Override
-    //        public void onCancelled(DatabaseError error) {
-    //            // Failed to read value
-    //            Log.w(TAG, "Failed to read value.", error.toException());
-    //        }
-    //    });
-    // Read from the database
-//         myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-//                String value = dataSnapshot.getValue(String.class);
-//                Log.d(TAG, "Value is: " + value);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w(TAG, "Failed to read value.", error.toException());
-//            }
-//        });
 
     Button goButton;
     int locationOfCorrectAnswer;
